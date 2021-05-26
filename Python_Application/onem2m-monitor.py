4ac66a5eb70c0c30c3d8ff8975c46c7201fc1fd2
BrSkat <brice.hechon@isen.yncrea.fr>
2021-05-26 09:05:34 +0200

    mise en place de la partie pluie dans le programme principale
    cela fonctionne, même si l'humidité est basse on n'arrose pas s'il pleut dans les 8 prochaines heure a la localisation prévu
    Tout est paramétrable dans le fichier de config le lieu et le nombre d'heures pour connaitre la pluie

--- Python_Application/onem2m-monitor.py
+++ Python_Application/onem2m-monitor.py
@@ -8,10 +8,12 @@ from flask import Response
 import sys
 import random
 import configparser
-import time
+from pyowm import OWM
+from pyowm.utils import timestamps
+from datetime import timedelta
 from pathlib import Path
 
-#/////////////// Parameters /////////////////
+# /////////////// Parameters /////////////////
 config = configparser.ConfigParser()
 path = Path(__file__).parent / "default.json"
 with open(path, 'r') as f:
@@ -34,66 +36,68 @@ requestNr = 0
 sensorToMonitor = ""
 actuatorToTrigger = ""
 
+
 def createSUB():
     global requestNr
     global cseRelease
     global poa_in_nu
     headers = {
-                    'Content-Type': 'application/json;ty=23',
-                    'X-M2M-Origin': monitorId,
-                    "X-M2M-RI": "req" + str(requestNr),
-                }
+        'Content-Type': 'application/json;ty=23',
+        'X-M2M-Origin': monitorId,
+        "X-M2M-RI": "req" + str(requestNr),
+    }
 
-    if(cseRelease != "1"):
-        headers.update({"X-M2M-RVI":cseRelease})
+    if (cseRelease != "1"):
+        headers.update({"X-M2M-RVI": cseRelease})
 
     notificationUri = [cseName + "/Monitor"]
-    if(poa_in_nu):
-    	notificationUri = [monitorPoA]
+    if (poa_in_nu):
+        notificationUri = [monitorPoA]
 
     response = requests.post(csePoA + '/' + cseName + "/" + sensorToMonitor + '/DATA',
-                json={
-                    "m2m:sub": {
-                        "rn": "SUB_Monitor",
-                        "nu": notificationUri,
-                        "nct": 1,
-                        "enc": {
-                            "net": [3]
-                        }
-                    }
-                }, 
-                headers=headers
-                )
+                             json={
+                                 "m2m:sub": {
+                                     "rn": "SUB_Monitor",
+                                     "nu": notificationUri,
+                                     "nct": 1,
+                                     "enc": {
+                                         "net": [3]
+                                     }
+                                 }
+                             },
+                             headers=headers
+                             )
     requestNr += 1
     if response.status_code != 201:
         print("SUB Creation error : ", response.text)
     else:
-        print("SUB Creation :" , response.status_code)
+        print("SUB Creation :", response.status_code)
+
 
 def createAE():
     global requestNr
     global cseRelease
     headers = {
-                    'Content-Type': 'application/json;ty=2',
-                    'X-M2M-Origin': monitorId,
-                    "X-M2M-RI": "req" + str(requestNr),
-                }
-    ae_json={
-			"m2m:ae":{
-					"rn": "Monitor", 
-					"api":"Norg.demo.monitor-app",
-					"rr":True,
-					"poa":[ monitorPoA ]
-				}
-			}
-    if(cseRelease != "1"):
-        headers.update({"X-M2M-RVI":cseRelease})
-        ae_json['m2m:ae'].update({"srv":[cseRelease]})
+        'Content-Type': 'application/json;ty=2',
+        'X-M2M-Origin': monitorId,
+        "X-M2M-RI": "req" + str(requestNr),
+    }
+    ae_json = {
+        "m2m:ae": {
+            "rn": "Monitor",
+            "api": "Norg.demo.monitor-app",
+            "rr": True,
+            "poa": [monitorPoA]
+        }
+    }
+    if (cseRelease != "1"):
+        headers.update({"X-M2M-RVI": cseRelease})
+        ae_json['m2m:ae'].update({"srv": [cseRelease]})
 
     response = requests.post(csePoA + "/" + cseName,
-                json=ae_json,
-                headers= headers
-                )
+                             json=ae_json,
+                             headers=headers
+                             )
     requestNr += 1
     if response.status_code != 201:
         print("AE Creation error : ", response.text)
@@ -101,26 +105,27 @@ def createAE():
         print("AE Creation :", response.status_code)
     createSUB()
 
+
 def createCIN(actuatorName, commandName):
     global requestNr
     global cseRelease
     headers = {
-                    'Content-Type': 'application/json;ty=4',
-                    'X-M2M-Origin': monitorId,
-                    "X-M2M-RI": "req" + str(requestNr),
-                }
+        'Content-Type': 'application/json;ty=4',
+        'X-M2M-Origin': monitorId,
+        "X-M2M-RI": "req" + str(requestNr),
+    }
 
     if (cseRelease != "1"):
         headers.update({"X-M2M-RVI": cseRelease})
 
     response = requests.post(csePoA + "/" + cseName + "/" + actuatorName + '/COMMAND',
-                json={
-                    "m2m:cin": {
-                        "con": commandName
-                    }
-                }, 
-                headers=headers
-                )
+                             json={
+                                 "m2m:cin": {
+                                     "con": commandName
+                                 }
+                             },
+                             headers=headers
+                             )
     requestNr += 1
     if response.status_code != 201:
         print("CIN Creation error : ", response.text)
@@ -130,56 +135,70 @@ def createCIN(actuatorName, commandName):
 
 api = Flask(__name__)
 
+
 @api.route('/', methods=['POST'])
 def processNotification():
     global isLedOn
     notificationJSON = request.json
     sensorValue = int(notificationJSON['m2m:sgn']['nev']['rep']['m2m:cin']['con'])
     print("Receieved sensor value : ", sensorValue)
-	
-	# demo selection : begin
+
+    # demo selection : begin
     if (sensorToMonitor == "HumiditySensor") and (actuatorToTrigger == "LedActuator"):
         commandLedHumidity(sensorValue)
     else:
         print("Demo not implemented")
-	# demo selection : end
-		
+    # demo selection : end
+
     response = Response('')
     response.headers["X-M2M-RSC"] = 2000
     if (cseRelease != "1"):
         response.headers["X-M2M-RVI"] = cseRelease
     return response
 
+
 def commandLedHumidity(sensorValue):
-    print("seuil humdité = %d"%(humidityThreshold))
+    print("seuil humdité = %d" % (humidityThreshold))
     global isLedOn
-    #if (sensorValue > humidityThreshold) and (isLedOn == True):
+
+    rainIn8hours = RainAtLocationInXHours(config['location']['city'] + ',' + config['location']['country'],  config['delay']['rainDelay'])
+
+    # if (sensorValue > humidityThreshold) and (isLedOn == True):
     if sensorValue > humidityThreshold:
         print("High humidity => Switch OFF the led")
         createCIN(actuatorToTrigger, "[switchOff]")
         isLedOn = False
-    #elif (sensorValue < humidityThreshold) and (isLedOn == False):
-    elif sensorValue < humidityThreshold:
-        print("Low humidity => Switch ON the led")
-        createCIN(actuatorToTrigger, "[switchOn]")
-        isLedOn = True
+    # elif (sensorValue < humidityThreshold) and (isLedOn == False):
+    elif (sensorValue < humidityThreshold) :
+        if rainIn8hours :
+            #pluie dans les 8 prochaines heures donc rien
+            print("Low humidity but rain in next "+str(config['delay']['rainDelay'])+" hours @"+config['location']['city']+" => Switch OFF the led")
+            createCIN(actuatorToTrigger, "[switchOff]")
+            isLedOn = False
+        else :
+            # pas de pluie dans les 8 prochaines heures donc on arrose
+            print("Low humidity and no rain in next "+str(config['delay']['rainDelay'])+" hours @"+config['location']['city']+" => Switch ON the led")
+            createCIN(actuatorToTrigger, "[switchOn]")
+            isLedOn = True
 
     else:
         print("Nothing to do")
 
+
 def commandActuator(args):
     global actuatorToTrigger
-    requestNr = random.randint(0,1000)
+    requestNr = random.randint(0, 1000)
     print("The command " + args.command + " will be sent to the actuator " + args.actuator)
-    actuatorToTrigger = args.actuator +"Actuator"
+    actuatorToTrigger = args.actuator + "Actuator"
     createCIN(actuatorToTrigger, "[" + args.command + "]")
     sys.exit()
+
 
 def getAll(args):
     global requestNr
     global cseRelease
 
-    requestNr = random.randint(0,1000)
+    requestNr = random.randint(0, 1000)
     print("Sending request >>> ")
     headers = {
         'X-M2M-Origin': monitorId,
@@ -189,7 +208,7 @@ def getAll(args):
     if (cseRelease != "1"):
         headers.update({"X-M2M-RVI": cseRelease})
 
-    response = requests.get(csePoA + "/" + cseName + "/" + args.sensor + "Sensor/DATA" +  '?rcn=4',
+    response = requests.get(csePoA + "/" + cseName + "/" + args.sensor + "Sensor/DATA" + '?rcn=4',
                             headers=headers)
 
     print("<<< Response received ! ")
@@ -204,10 +223,11 @@ def getAll(args):
 
     sys.exit()
 
+
 def getLatest(args):
     global requestNr
     global cseRelease
-    requestNr = random.randint(0,1000)
+    requestNr = random.randint(0, 1000)
     print("Sending request >>> ")
     headers = {
         'X-M2M-Origin': monitorId,
@@ -231,44 +251,65 @@ def getLatest(args):
 
     sys.exit()
 
+
 def getParameters():
-   import argparse
-   global sensorToMonitor, actuatorToTrigger
+    import argparse
+    global sensorToMonitor, actuatorToTrigger
 
-   #Command-line parsing
-   parser = argparse.ArgumentParser()
-   parser.add_argument("-s","--sensor", choices=["Humidity"], default="Humidity", help='Sensor to be monitored')
-   parser.add_argument("-a","--actuator", choices=["Led", "Display", "Servo"],default="Led", help='Actuator to trigger')
+    # Command-line parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--sensor", choices=["Humidity"], default="Humidity", help='Sensor to be monitored')
+    parser.add_argument("-a", "--actuator", choices=["Led", "Display", "Servo"], default="Led",
+                        help='Actuator to trigger')
 
-   #Subcommands command-line parsing
-   subparsers = parser.add_subparsers(required=False, help="Subcommands")
-   #"commandActuator"
-   parser_commandActuator = subparsers.add_parser("commandActuator", help="Force a command on the given actuator")
-   parser_commandActuator.add_argument('command', help='Given command')
-   parser_commandActuator.add_argument("-a","--actuator", choices=["Led"],default="Led", help='Given actuator')
-   parser_commandActuator.set_defaults(func=commandActuator)
-   #"getAll"
-   parser_getAll = subparsers.add_parser("getAll", help="Get all data from the given sensor")
-   parser_getAll.add_argument('-s',"--sensor", choices=["Humidity"], default="Humidity", help='Given sensor')
-   parser_getAll.set_defaults(func=getAll)
-   #"getLatest"
-   parser_getLatest = subparsers.add_parser("getLatest", help="Get latest data from the given sensor")
-   parser_getLatest.add_argument('-s', "--sensor", choices=["Humidity"], default="Humidity", help='Given sensor')
-   parser_getLatest.set_defaults(func=getLatest)
+    # Subcommands command-line parsing
+    subparsers = parser.add_subparsers(required=False, help="Subcommands")
+    # "commandActuator"
+    parser_commandActuator = subparsers.add_parser("commandActuator", help="Force a command on the given actuator")
+    parser_commandActuator.add_argument('command', help='Given command')
+    parser_commandActuator.add_argument("-a", "--actuator", choices=["Led"], default="Led", help='Given actuator')
+    parser_commandActuator.set_defaults(func=commandActuator)
+    # "getAll"
+    parser_getAll = subparsers.add_parser("getAll", help="Get all data from the given sensor")
+    parser_getAll.add_argument('-s', "--sensor", choices=["Humidity"], default="Humidity", help='Given sensor')
+    parser_getAll.set_defaults(func=getAll)
+    # "getLatest"
+    parser_getLatest = subparsers.add_parser("getLatest", help="Get latest data from the given sensor")
+    parser_getLatest.add_argument('-s', "--sensor", choices=["Humidity"], default="Humidity", help='Given sensor')
+    parser_getLatest.set_defaults(func=getLatest)
 
-   args = parser.parse_args()
+    args = parser.parse_args()
 
-   print(args)
+    print(args)
 
-   sensorToMonitor = args.sensor + "Sensor"
-   actuatorToTrigger = args.actuator + "Actuator"
+    sensorToMonitor = args.sensor + "Sensor"
+    actuatorToTrigger = args.actuator + "Actuator"
 
-   if args.__contains__("func"):
-    args.func(args)
+    if args.__contains__("func"):
+        args.func(args)
+
+
+def RainAtLocationInXHours(location, hours):
+    # API Setup
+    APIKEY = config["API"]['key']
+    owm = OWM(APIKEY)
+    mgr = owm.weather_manager()
+
+    # On creer une variable contenant les previsions mise a jours toute les 3h
+    three_h_forecaster = mgr.forecast_at_place(location, '3h')
+
+    # on creer une variable temps qui contient le temps dans X heures
+    inXHours = timestamps.now() + timedelta(hours=hours)
+
+    # On recupere l'info sur la pluie
+    rain = three_h_forecaster.will_be_rainy_at(inXHours)
+
+    # On renvoie un booleen selon s'il va pleuvoir a cette lcation dans les x procchaines heures
+    return rain
+
 
 if __name__ == '__main__':
     getParameters()
-
 
     if (sensorToMonitor == "HumiditySensor"):
         humidityThreshold = humidityThreshold
